f5c029c34f7f7e2c6f51e561104fd518472365f1
P4yl04d <1501608414@qq.com>
2023-12-08 23:50:08 +0800

    Archlinux

---
+++ Java/Leetcode005/2/Solution.java
@@ -0,0 +1,17 @@
+class Soluion
+{
+    public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int n1 = l1 != null ? l1.val:0;
+            int n2 = l2 != null ? l2.val:0;
+            int carry = 0;
+            int sum = n1 + n2 + carry;
+            if(head == null && tail == null){
+
+            }
+        }
+    }
+}
