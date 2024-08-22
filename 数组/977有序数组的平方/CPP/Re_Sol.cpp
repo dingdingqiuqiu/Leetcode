26c1922f7d5c28b6723d6ba01acfe85c55ab86eb
P4yl04d <1501608414@qq.com>
2024-08-22 11:44:36 +0800

    有序数组的平方(CPP重构完成)

---
+++ 数组/977有序数组的平方/CPP/Re_Sol.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size(),0);
+        // res = {0,1,9,16,100};
+        int left = 0;
+        int right = nums.size() - 1;
+        int i = 1;
+        while(left <= right) {
+            if(nums[left]*nums[left] > nums[right]*nums[right]) {
+                res[nums.size()-i] = nums[left]*nums[left];
+                // cout << "left:" << left << endl;
+                left++;
+            } else {
+                res[nums.size()-i] = nums[right]*nums[right];
+                // cout << "right:" << right << endl;
+                right--;
+            } 
+            i++;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-4,-1,0,3,10};
+    vector<int> exp = {0,1,9,16,100};
+    vector<int> res;
+    res = sol.sortedSquares(nums);
+    for (int i = 0; i < res.size(); i++) {
+        if (res[i] == exp[i]) {
+            cout << "第" << i << "项匹配成功" << endl;
+        }
+    }
+}
